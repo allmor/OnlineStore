98543c0edef6d780a9755febe99f89ed202eaa3a
Aleksey Morozov <allmor@mail.ru>
2022-08-16 12:09:12 +0400

    add repositories/ProductRepository.java

---
+++ src/main/java/com/onlinestoreapp/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.onlinestoreapp.repositories;
+
+import com.onlinestoreapp.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
