cc022fbc5821c403650ac003d4ab76bbbbfa72cf
Aleksey Morozov <allmor@mail.ru>
2022-08-16 10:32:17 +0400

    initial commit

---
+++ src/main/java/com/onlinestoreapp/OnlinestoreappApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinestoreappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinestoreappApplication.class, args);
+	}
+
+}
