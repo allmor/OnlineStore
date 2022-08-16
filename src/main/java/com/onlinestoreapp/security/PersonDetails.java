32ec36d40afe7242924fb7df7b7321ba8e034f3f
Aleksey Morozov <allmor@mail.ru>
2022-08-16 12:21:28 +0400

    add security/PersonDetails.java

---
+++ src/main/java/com/onlinestoreapp/security/PersonDetails.java
@@ -0,0 +1,52 @@
+package com.onlinestoreapp.security;
+
+import com.onlinestoreapp.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    @Autowired
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.person.getPersonEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
