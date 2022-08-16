94d15101f3a0b11865fcffc404297255696f1404
Aleksey Morozov <allmor@mail.ru>
2022-08-16 12:24:17 +0400

    add services/PersonDetailsService.java

---
+++ src/main/java/com/onlinestoreapp/services/PersonDetailsService.java
@@ -0,0 +1,34 @@
+package com.onlinestoreapp.services;
+
+import com.onlinestoreapp.models.Person;
+import com.onlinestoreapp.repositories.PeopleRepository;
+import com.onlinestoreapp.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByPersonEmail(username);
+
+        if (person.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            return new PersonDetails(person.get());
+        }
+    }
+}
