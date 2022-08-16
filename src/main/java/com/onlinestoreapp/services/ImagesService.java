4157d5ff06489b85bdd3f75dcaf06f9708e40f7f
Aleksey Morozov <allmor@mail.ru>
2022-08-16 12:13:33 +0400

    add services/ImagesService.java

---
+++ src/main/java/com/onlinestoreapp/services/ImagesService.java
@@ -0,0 +1,17 @@
+package com.onlinestoreapp.services;
+
+import com.onlinestoreapp.repositories.ImagesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ImagesService {
+    private final ImagesRepository imagesRepository;
+
+    @Autowired
+    public ImagesService(ImagesRepository imagesRepository) {
+        this.imagesRepository = imagesRepository;
+    }
+}
