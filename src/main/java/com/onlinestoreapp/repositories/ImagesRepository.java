f48472a666b16f142a5240bb93d053e193061a44
Aleksey Morozov <allmor@mail.ru>
2022-08-16 12:03:57 +0400

    add repositories/ImagesRepository.java

---
+++ src/main/java/com/onlinestoreapp/repositories/ImagesRepository.java
@@ -0,0 +1,9 @@
+package com.onlinestoreapp.repositories;
+
+import com.onlinestoreapp.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImagesRepository extends JpaRepository<Image, Integer> {
+}
