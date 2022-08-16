ed9cfcdf9a7b7120880387f9410470fa4f6ec36a
Aleksey Morozov <allmor@mail.ru>
2022-08-16 12:38:38 +0400

    add controllers/ImageController.java

---
+++ src/main/java/com/onlinestoreapp/controllers/ImageController.java
@@ -0,0 +1,35 @@
+package com.onlinestoreapp.controllers;
+
+import com.onlinestoreapp.models.Image;
+import com.onlinestoreapp.repositories.ImagesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+
+@RestController
+public class ImageController {
+
+    private final ImagesRepository imagesRepository;
+
+    @Autowired
+    public ImageController(ImagesRepository imagesRepository) {
+        this.imagesRepository = imagesRepository;
+    }
+
+    @GetMapping("/images/{id}")
+    public ResponseEntity<?> getImageById(@PathVariable("id") Integer id) {
+        Image image = imagesRepository.findById(id).orElse(null);
+
+        return ResponseEntity.ok()
+                .header("fileName", image.getFileName())
+                .contentType(MediaType.valueOf(image.getContentType()))
+                .contentLength(image.getSize())
+                .body(new InputStreamResource(new ByteArrayInputStream(image.getBytes())));
+    }
+}
