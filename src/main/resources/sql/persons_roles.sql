b12f56b78501d993c339a362d857ededbc83eaaf
Aleksey Morozov <allmor@mail.ru>
2022-09-15 14:19:14 +0400

    added and edited sql commands

--- src/main/resources/sql/persons_roles.sql
+++ src/main/resources/sql/persons_roles.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS persons_roles;
 
 CREATE TABLE IF NOT EXISTS persons_roles
 (
-    person_credentials_id INT NOT NULL REFERENCES person_credentials(id),
+    person_id INT NOT NULL REFERENCES persons(person_id),
     roles_id INT NOT NULL REFERENCES roles(id)
 );
